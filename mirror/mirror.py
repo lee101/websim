7dc9b8deda4777cc87cc5a5bea9a15f52fd4d9b4
Lee Penkman <leepenkman@gmail.com>
2015-01-11 08:48:19 +1300

    Add Google analytics to proxy server

--- mirror/mirror.py
+++ mirror/mirror.py
@@ -254,6 +254,25 @@ class MirrorHandler(BaseHandler):
             fiddle = Fiddle.byUrlKey(fiddle_name)
             self.response.out.write('<script id="webfiddle-js">' + fiddle.script + '</script>')
             self.response.out.write('<style id="webfiddle-css">' + fiddle.style + '</style>')
+            self.response.out.write("""
+<script>
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-57646272-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
+            """)
 
 ###############################################################################
 
