48ebb63055b38cb2d4a56f91b743737131678351
Lee Penkman <leepenkman@gmail.com>
2016-04-18 12:02:57 +1200

    Remove chikita ads
    They don't make any money

--- mirror/mirror.py
+++ mirror/mirror.py
@@ -195,23 +195,10 @@ class HomeHandler(BaseHandler):
         self.response.out.write(template.render("main.html", context))
 
 
-add_code = """<div style="min-width:550px;min-height:250px;width:100%">
-<script type="text/javascript">
-( function() {
-if (window.CHITIKA === undefined) { window.CHITIKA = { 'units' : [] }; };
-var unit = {"calltype":"async[2]","publisher":"leepenkman","width":550,"height":250,"sid":"Chitika Default"};
-var placement_id = window.CHITIKA.units.length;
-window.CHITIKA.units.push(unit);
-document.write('<div id="chitikaAdBlock-' + placement_id + '"></div>');
-}());
-</script>
-<script type="text/javascript" src="//cdn.chitika.net/getads.js" async></script>
-
-</div>"""
+add_code = """"""
 
 big_add_code = """<iframe style="min-width:600px;min-height:800px;width:100%" src="http://www.addictingwordgames.com">
-    <script id="mNCC" language="javascript">  medianet_width='600';  medianet_height= '250';  medianet_crid='275317118';  </script>  <script id="mNSC" src="http://contextual.media.net/nmedianet.js?cid=8CUV76NDB" language="javascript"></script>
-</iframe>"""
+    </iframe>"""
 
 def request_blocker(fiddle_name):
     return """
